8ab250e16dbafe73a833f725204f686c98629c8b
jmthon20au <158489980+jmthon20au@users.noreply.github.com>
2024-03-13 19:40:19 +0300

    Add files via upload

---
+++ قراءة اللملفات.py
@@ -0,0 +1,32 @@
+"""
+by means of @DF_GD_D
+in a 2023/12/14
+Channel : @T62RS
+""" 
+import telebot  #مكتبة
+
+private = "6436588079:AAFUS6vefmPvMX6VYyQQEJfQIL2iOsD3Cro"#توكنك
+bot = telebot.TeleBot(private)
+is_bot_active = True #التشغيل 
+bot.set_my_commands([telebot.types.BotCommand("/start", " 🤖 𝗦𝗧𝗔𝗥𝗧 𝗕𝗢𝗧 ")]) 
+#الكوماند
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message,f"""
+    🌺 مرحباً بك عزيزي في بوت قراءة الملفات بصيغة ( txt ) وارسال النص في البوت الرجاء ارسال الملف للقراءة ✍ 📨
+
+*البوت لا يدعم ملفات PDF"""
+@bot.message_handler(content_types=["document"])
+def handle_document(message):
+   file_info = bot.get_file(message.document.file_id)
+   downloaded_file = bot.download_file(file_info.file_path)
+   file_name = message.document.file_name
+   text = downloaded_file.decode("utf-8")
+   bot.reply_to(message, "• النـص داخـل الـمـلف  {} هـو => \n\n {}".format(file_name, text))
+
+print("تم ✅✅") 
+bot.polling(none_stop=True)
+"""
+✨ by means of @re_file_bot
+🔰 Channel : @my00002
+""" 
